ec091ebdd266d2783b2beb9412727a4f326f9488
000raihan <000raihanahmad@gmail.com>
2022-03-20 23:02:12 +0600

    navbar & hero section complete

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+    fontFamily:{
+      mulish:['Mulish'],
+      raleway:['Raleway'],
+      maven:['Maven Pro']
+    },
+  
+    extend: {
+      colors:{
+        "deep-green" : "#0E7139",
+        "t-green" : "#0E7139",
+        "light-yellow" : "#FDBD12",
+        "light-red": "#DD213F",
+        "bold-black" : "#444444",
+        "thin-black" : "#737373",
+        "bold-black-2" : "#222222"
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
